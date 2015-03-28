34d3c51be7eefc93506be6c56020c392fbd5f561
Calvin Cheng <linchuan.cheng@gmail.com>
2015-03-28 19:36:05 +0800

    Recap on using the os module

---
+++ system/os-examples.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var os = require('os');
+
+console.log(os.hostname());
+
+console.log(os.tmpDir());
+
+console.log(os.platform());
+
+console.log(os.type());
+
+console.log(os.release());
+
+console.log(os.arch());

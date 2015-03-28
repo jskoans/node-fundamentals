8c98c6836d96ed75c57df47a4504c5cc950bbb30
Calvin Cheng <linchuan.cheng@gmail.com>
2015-03-28 18:15:31 +0800

    module.exports, npm modules and require fun

---
+++ modules/npm-modules.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var request = require('request');
+
+request('http://www.google.com', function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    console.log(body);
+  }
+});
